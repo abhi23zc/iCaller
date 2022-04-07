e57432090a0281ab07edd504819e6f6fbb0105e4
unknown <zrfabhi8@gmail.com>
2022-04-06 22:23:49 -0700

    abhishek

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { response } = require('express');
 const express = require('express')
-const truecallerjs  = require('truecallerjs')
+const truecallerjs = require('truecallerjs')
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 80
 const app = express();
@@ -12,10 +12,10 @@ var mynumber = ""
 
 
 
-app.get('/', (req , res) =>{
-    res.render('data', {name: "name", carrier:"carrier", city:"city",  timezone:"timezone", email:"Email", code:"Dialing Code"})
+app.get('/', (req, res) => {
+    res.render('data', { name: "name", carrier: "carrier", city: "city", timezone: "timezone", email: "Email", code: "Dialing Code" })
     console.log(req.body.myfile)
-    
+
 })
 
 // app.post('/', (req, res)=>{
@@ -24,72 +24,93 @@ app.get('/', (req , res) =>{
 //     console.log(mynumber)
 // })
 
-    
 
 
-app.post('/', (req, res) =>{
 
-    let searchData = {
-        number: req.body.myfile,
-        countryCode: "IN",
-        installationId: "a1i0B--YFvJq5-mVMgccpJGeu0qlV9di0kNNTn345hXlCPdPeSGVxNf_K6ICEk4_",
-        output: "JSON"
+app.post('/', (req, res) => {
+
+
+    var new_number = Number(req.body.myfile)
+    console.log(new_number)
+
+    if (isNaN(new_number)) {
+        res.redirect('https://icaller.herokuapp.com/')
+
     }
-    
-    let sn = truecallerjs.searchNumber(searchData);
-    sn.then(function(response) {
+    else {
 
-        console.log(req.body.myfile)
 
-        
-        var obj = JSON.parse(response)
-        let name = obj.data[0].name
-        let carrier  = obj.data[0].phones[0].carrier
-        let dialingCode = obj.data[0].phones[0].dialingCode
-        
-        let city =  obj.data[0].addresses[0].city
-        let timeZone = obj.data[0].addresses[0].timeZone
-        console.log(obj.data[0].name)
-        console.log(obj.data[0].phones[0].carrier)
-        console.log(obj.data[0].phones[0].dialingCode)
-      
-        console.log(obj.data[0].phones[0].countryCode)
-        console.log(obj.data[0].addresses[0].city)
-        console.log(obj.data[0].addresses[0].timeZone)
 
-        try{
 
-            const email = obj.data[0].internetAddresses[0].id
-            if(email == null){
-
-                res.render('data', {name: name, carrier:carrier, city:city,  timezone:timeZone, email:"No email provided",code:dialingCode})
-            }
-            
-            else{
-                res.render('data', {name: name, carrier:carrier, city:city,  timezone:timeZone, email:email,code:dialingCode})
-                
-            }
-            
-        }catch{
-            
-            res.render('data', {name: name, carrier:carrier, city:city,  timezone:timeZone, email:"No data Provided",code:dialingCode})
+        let searchData = {
+            number: req.body.myfile,
+            countryCode: "IN",
+            installationId: "a1i0B--YFvJq5-mVMgccpJGeu0qlV9di0kNNTn345hXlCPdPeSGVxNf_K6ICEk4_",
+            output: "JSON"
         }
 
-            
-        
-        
-
-
-    
-    
-
-});
+        let sn = truecallerjs.searchNumber(searchData);
+        sn.then(function (response) {
 
 
 
-    
+
+
+
+
+            var obj = JSON.parse(response)
+            let name = obj.data[0].name
+            let carrier = obj.data[0].phones[0].carrier
+            let dialingCode = obj.data[0].phones[0].dialingCode
+
+            let city = obj.data[0].addresses[0].city
+            let timeZone = obj.data[0].addresses[0].timeZone
+            console.log(obj.data[0].name)
+            console.log(obj.data[0].phones[0].carrier)
+            console.log(obj.data[0].phones[0].dialingCode)
+
+            console.log(obj.data[0].phones[0].countryCode)
+            console.log(obj.data[0].addresses[0].city)
+            console.log(obj.data[0].addresses[0].timeZone)
+
+            try {
+
+                const email = obj.data[0].internetAddresses[0].id
+                if (email == null) {
+
+                    res.render('data', { name: name, carrier: carrier, city: city, timezone: timeZone, email: "No email provided", code: dialingCode })
+                }
+
+                else {
+                    res.render('data', { name: name, carrier: carrier, city: city, timezone: timeZone, email: email, code: dialingCode })
+
+                }
+
+            } catch {
+
+                res.render('data', { name: name, carrier: carrier, city: city, timezone: timeZone, email: "No data Provided", code: dialingCode })
+            }
+
+
+
+
+
+
+
+
+
+
+        });
+
+    }
+
+
+
+
+
+
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log("app listening")
 })
